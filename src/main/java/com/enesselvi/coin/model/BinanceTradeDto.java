123704bcdfce2a98f36a5f726c02eba30e594b58
enes.selvili <enes.selvili@eteration.com>
2025-10-14 16:17:02 +0300

    feat: add DTO class for Binance trade JSON data

---
+++ src/main/java/com/enesselvi/coin/model/BinanceTradeDto.java
@@ -0,0 +1,106 @@
+package com.enesselvi.coin.model;
+
+import java.math.BigDecimal;
+
+/**
+{
+    wss://stream.binance.com:9443/ws/btcusdt@trade this websocket returns this json data:
+ | Alan | Tür                    | Açıklama                                                                                                            |
+ | ---- | ---------------------- | ------------------------------------------------------------------------------------------------------------------- |
+ | `e`  | String                 | **Event type** → Olayın türü. Burada `"trade"` yani bu mesaj bir “işlem” olayıdır.                                  |
+ | `E`  | Number (timestamp, ms) | **Event time** → Olayın Binance sunucusunda gerçekleştiği zaman (Unix epoch milliseconds).                          |
+ | `s`  | String                 | **Symbol** → İşlem yapılan parite. Örneğin `"BTCUSDT"`.                                                             |
+ | `t`  | Number                 | **Trade ID** → Binance sisteminde bu işleme verilen benzersiz kimlik numarası.                                      |
+ | `p`  | String (decimal)       | **Price** → İşlemin gerçekleştiği fiyat (örneğin `"111140.01"` USDT).                                               |
+ | `q`  | String (decimal)       | **Quantity** → İşlemde el değiştiren coin miktarı (örneğin `"0.00262"` BTC).                                        |
+ | `T`  | Number (timestamp, ms) | **Trade time** → İşlemin gerçekten gerçekleştiği zaman. Genellikle `E` ile aynıdır ama küçük fark olabilir.         |
+ | `m`  | Boolean                | **Is buyer the market maker?** → `true` ise alıcı market maker’dır. (Yani limit emrini veren kişi alıcı tarafıdır.) |
+ | `M`  | Boolean                | **Ignore field** → Binance dokümantasyonuna göre bu alan **her zaman true** olur ve **yoksayılabilir**.             |
+
+ Example JSON Data:
+
+        "e": "trade",
+        "E": 1760444202088,
+        "s": "BTCUSDT",
+        "t": 5337029739,
+        "p": "111140.01000000",
+        "q": "0.00262000",
+        "T": 1760444202088,
+        "m": true,
+        "M": true
+}
+ */
+public class BinanceTradeDto {
+    private  String     e_eventType;
+    private  Long     E_number;
+    private  String       s_symbol;
+    private  Long       t_tradeId;
+    private  BigDecimal p_price;
+    private  BigDecimal q_quantity;
+    private  Long       T_tradeTime;
+    private  Boolean    m_marketMaker;
+
+
+    public String getE_eventType() {
+        return e_eventType;
+    }
+    public void setE_eventType(String e_eventType) {
+        this.e_eventType = e_eventType;
+    }
+
+
+    public String getS_symbol() {
+        return s_symbol;
+    }
+    public void setS_symbol(String s_symbol) {
+        this.s_symbol = s_symbol;
+    }
+
+
+    public Long getE_number() {
+        return E_number;
+    }
+    public void setE_number(Long e_number) {
+        E_number = e_number;
+    }
+
+
+    public Long getT_tradeId() {
+        return t_tradeId;
+    }
+    public void setT_tradeId(Long t_tradeId) {
+        this.t_tradeId = t_tradeId;
+    }
+
+
+    public BigDecimal getP_price() {
+        return p_price;
+    }
+    public void setP_price(BigDecimal p_price) {
+        this.p_price = p_price;
+    }
+
+
+    public BigDecimal getQ_quantity() {
+        return q_quantity;
+    }
+    public void setQ_quantity(BigDecimal q_quantity) {
+        this.q_quantity = q_quantity;
+    }
+
+
+    public Long getT_tradeTime() {
+        return T_tradeTime;
+    }
+    public void setT_tradeTime(Long t_tradeTime) {
+        T_tradeTime = t_tradeTime;
+    }
+
+
+    public Boolean getM_marketMaker() {
+        return m_marketMaker;
+    }
+    public void setM_marketMaker(Boolean m_marketMaker) {
+        this.m_marketMaker = m_marketMaker;
+    }
+}
